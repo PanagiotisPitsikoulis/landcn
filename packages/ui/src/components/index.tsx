196413f7d377394f6fc6ced8833c82226fe1a5c3
Panagiotis Pitsikoulis <panagiotispitsikoulis2003@gmail.com>
2025-01-03 02:27:31 +0200

    removed chart and siebar

--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 export * from "./carousel";
-// export * from "./chart";
 export * from "./grid";
 export * from "./gridItem";
 export * from "./iphone";
 export * from "./polymorphic-button";
 export * from "./safari";
-// export * from "./sidebar";
 export * from "./theme-switch";
 export * from "./top-banner";
 export * from "./virtual-anchor";
